b46375f05dd3c69c163f7a02b8f0c7dab91440ea
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-07-23 12:47:43 -0400

    Added more AlgorithmIdentifierTests

--- kjwt-core/src/commonTest/kotlin/com.mooncloak.kodetools.kjwt.core/crypto/AlgorithmIdentifierTest.kt
+++ kjwt-core/src/commonTest/kotlin/com.mooncloak.kodetools.kjwt.core/crypto/AlgorithmIdentifierTest.kt
@@ -65,7 +65,7 @@ class AlgorithmIdentifierTest {
     }
 
     @Test
-    fun expectedEncodings() {
+    fun md2_encodes_correctly() {
         // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
         // Notes: 1.
         assertContentEquals(
@@ -86,6 +86,209 @@ class AlgorithmIdentifierTest {
                 0x00
             ),
             actual = AlgorithmIdentifier.MD2.encode()
+        )
+    }
+
+    @Test
+    fun md5_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0C, // length 12
+                0x06, // OID TAG
+                0x08, // length 8
+                0x2A,
+                0x86.toByte(),
+                0x48,
+                0x86.toByte(),
+                0xF7.toByte(),
+                0x0D,
+                0x02,
+                0x05, // OID value (first part)
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.MD5.encode()
+        )
+    }
+
+    @Test
+    fun sha1_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x09, // length
+                0x06, // OID TAG
+                0x05, // length
+                0x2B,
+                0x0E,
+                0x03,
+                0x02,
+                0x1A,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA1.encode()
+        )
+    }
+
+    @Test
+    fun sha224_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x04,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA224.encode()
+        )
+    }
+
+    @Test
+    fun sha256_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x01,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA256.encode()
+        )
+    }
+
+    @Test
+    fun sha384_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x02,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA384.encode()
+        )
+    }
+
+    @Test
+    fun sha512_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x03,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA512.encode()
+        )
+    }
+
+    @Test
+    fun sha512_224_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x05,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA512_224.encode()
+        )
+    }
+
+    @Test
+    fun sha512_256_encodes_correctly() {
+        // https://www.rfc-editor.org/rfc/rfc8017#section-9.2
+        // Notes: 1.
+        assertContentEquals(
+            expected = byteArrayOf(
+                0x30, // SEQUENCE TAG
+                0x0d, // length
+                0x06, // OID TAG
+                0x09, // length
+                0x60,
+                0x86.toByte(),
+                0x48,
+                0x01,
+                0x65,
+                0x03,
+                0x04,
+                0x02,
+                0x06,
+                0x05, // NULL PARAMETERS
+                0x00
+            ),
+            actual = AlgorithmIdentifier.SHA512_256.encode()
         )
     }
 }
