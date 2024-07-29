166c6bb93b46acb616092cce66153e966caff29a
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-07-29 17:53:30 -0400

    Removed platform-specific signature algorithm signing key generation functions

--- kjwt-core/src/jsMain/kotlin/com.mooncloak.kodetools.kjwt.core/JsSigningUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.mooncloak.kodetools.kjwt.core
-
-import com.mooncloak.kodetools.kjwt.core.key.Jwk
-import com.mooncloak.kodetools.kjwt.core.signature.SignatureAlgorithm
-import com.mooncloak.kodetools.kjwt.core.util.ExperimentalJwtApi
-import kotlinx.serialization.json.Json
-
-@ExperimentalJwtApi
-public actual suspend fun SignatureAlgorithm.generateSigningKey(json: Json): Jwk? = TODO()
